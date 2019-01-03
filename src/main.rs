3453113a8385df01a52a7b0e4764dc58ebacaf39
Todd Hainsworth <hainsworth.todd@gmail.com>
2019-01-03 22:20:35 +1030

    add usage text + default priority

--- src/main.rs
+++ src/main.rs
@@ -81,20 +81,26 @@ fn main() {
                 process::exit(1);
             }
         };
+    // Print usage
+    } else if args.len() == 3 && args[1] == "-h" {
+        print_usage(&args);
+        process::exit(0);
     // Add a new item
-    } else if args.len() == 3 {
+    } else if args.len() >= 2 {
         let text = &args[1];
-        let priority = &args[2];
-        let priority = match priority.parse::<usize>() {
-            Ok(priority) => priority,
-            Err(e) => {
-                eprintln!("Priority must be a number (1, 2 or 3): {}", e);
-                process::exit(1);
-            }
-        };
-        items.push(TodoItem::new(text, false, priority));
-    } else {
-        // todo: print usage
+        // 3 args means we have a priority
+        if args.len() == 3 {
+            let priority = &args[2];
+            match priority.parse::<usize>() {
+                Ok(priority) => items.push(TodoItem::new(text, false, priority)),
+                Err(e) => {
+                    eprintln!("Priority must be a number (1, 2 or 3): {}", e);
+                    process::exit(1);
+                }
+            };
+        } else {
+            items.push(TodoItem::new(text, false, 1));
+        }
     }
 
     match update_todo_file(&items) {
@@ -132,4 +138,21 @@ fn update_todo_file(items: &Vec<TodoItem>) -> Result<()> {
     let path = get_todo_file_path();
     let buf = serde_json::to_string(&items).unwrap();
     fs::write(path, buf)
+}
+
+fn print_usage(args: &Vec<String>) {
+    println!("USAGE: {0} ...
+
+EXAMPLES:
+Show current items
+- {0}
+Add a new item
+- {0} \"Do something cool\"
+- {0} \"Submit report for XYZ\" 1 # top priority
+- {0} \"Get milk on the way home\" 4 # lower priority
+Mark an existing item as complete
+- {0} -c <item-id> # get this by running '{0}' on it's own
+Delete an existing item from the list
+- {0} -d <item-id> # get this by running '{0}' on it's own
+    ", args[0]);
 }
